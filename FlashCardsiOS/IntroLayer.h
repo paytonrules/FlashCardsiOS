c393b909c19e15ce80776c99c8f6f4b0976cbf28
paytonrules <esmith@8thlight.com>
2013-07-31 23:34:08 -0500

    Commit generated project

---
+++ FlashCardsiOS/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  FlashCardsiOS
+//
+//  Created by Eric Smith on 7/31/13.
+//  Copyright Eric Smith 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
